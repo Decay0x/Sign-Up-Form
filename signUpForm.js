093a96803034f24f1672b1c6a142eeb2290eff69
Decay0x <140253856+Decay0x@users.noreply.github.com>
2023-10-22 11:07:07 +0100

    min character validation added.Password input comparisson with js.Tidy up the look on mobile view.

---
+++ signUpForm.js
@@ -0,0 +1,37 @@
+const passwordInput  = document.getElementById("password")
+const confPassInput = document.getElementById("confPassword")
+let password = "";
+let confPass = "";
+
+const span = document.createElement("span")
+const passPopUp = document.getElementById("passPopUp")
+span.style.cssText = "color: red; font-size: 6px;"
+span.textContent = "*Passwords do not match"
+passPopUp.appendChild(span);
+
+passwordInput.addEventListener("input", (e)=>{
+    password = (passwordInput.value)
+})
+
+confPassInput.addEventListener("input", (e)=>{
+    confPass = (confPassInput.value)
+})
+
+
+document.querySelectorAll("input[type='password']").forEach(input=>{
+    passwordInput.style.cssText = "border:1px solid red";
+    confPassInput.style.cssText = "border:1px solid red";
+    input.addEventListener("input", (e)=>{
+    if (password != confPass ) {
+            e.preventDefault();
+            passwordInput.style.cssText = "border:1px solid red";
+            confPassInput.style.cssText = "border:1px solid red";
+       } else {
+            passwordInput.style.border = "1px solid green";
+            passwordInput.style.boxShadow = "1px 1px 4px green"; 
+            confPassInput.style.border = "1px solid green";
+            confPassInput.style.boxShadow = "1px 1px 4px green";
+            passPopUp.removeChild(span)
+       }  
+    })
+})
